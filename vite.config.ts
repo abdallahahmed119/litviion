824a3e561f4be10ce7109bdd8ab0e289901d0018
3bdallah <149585546+abdallahahmed119@users.noreply.github.com>
2026-03-24 11:22:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: '/LITVISION/',
+  base: '/litviion/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
